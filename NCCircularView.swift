f8ac8bf59151c5366cf97cb9da93e80d7d52b3a2
Nadeesha Lakmal <nadeeshac32@gmail.com>
2024-02-07 13:24:54 +0800

    added NCCircularView

---
+++ NCCircularView.swift
@@ -0,0 +1,24 @@
+//
+//  NCCircularView.swift
+//  NCCircularView
+//
+//  Created by Nadeesha Chandrapala on 07/02/2024.
+//
+
+public class NCCircularView: UIView {
+        
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupView() {
+        self.layer.cornerRadius = self.bounds.width / 2
+        self.layer.masksToBounds = true
+        self.clipsToBounds = true
+    }
+}
